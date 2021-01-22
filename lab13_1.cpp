fea114fcc68447eae6e94431ec33e2b0496284de
earthx15 <uncles1512@hotmail.com>
2021-01-22 16:48:00 +0700

    pass

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -3,7 +3,44 @@ using namespace std;
 
 template <typename T>
 void insertionSort(T d[],int N){
-
+	T temp;
+	for (int i = 1; i < N; i++)
+	{	
+		int count=0;
+		temp=d[i];
+		for (int j = i-1; j >= 0; j--)
+		{	
+			if (temp>d[j])
+			{
+				if (i==1)
+				{
+					d[j+1]=d[j];
+					d[j]=temp;
+					count++;
+				}else{
+					if (j==0)
+					{
+						d[j+1]=d[j];
+						d[j]=temp;
+					}else{d[j+1]=d[j];}
+					count++;
+				}
+				count++;
+			}else{
+				if (count==0)
+				{
+					break;
+				}else{
+					d[j+1]=temp;
+					break;
+				}
+			}
+		}
+		cout << "Pass " << i<<":";
+		for(int i = 0; i < 10; i++) cout << d[i] << " ";
+		cout <<"\n";
+		
+	}
 }
 
 int main(){
